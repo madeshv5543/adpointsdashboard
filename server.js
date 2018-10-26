48a6e2d20d9be358cf166e185e6c3c92c8240a4d
madeshv5543 <madeshv5543@gmail.com>
2018-10-26 17:23:33 +0530

    serve

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const serveStatic = require('serve-static')
+const path = require('path')
 app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
 const port = 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
