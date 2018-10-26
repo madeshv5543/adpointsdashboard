251c564efa607f13f327193722f7a6f70e4172c7
madeshv5543 <madeshv5543@gmail.com>
2018-10-26 16:40:50 +0530

    change port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 app.use(express.static('dist')) // absolute or relative to CWD
-const port = 8080
+const port = 8081
 app.listen(port, () => console.log(`Listening on port ${port}`))
