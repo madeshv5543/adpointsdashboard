72bbb85c08850afed6ae1331ebb4bf3161bac479
root <root@ip-172-31-28-133.ap-southeast-1.compute.internal>
2018-10-26 12:13:27 +0000

    histroy fallback

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const history = require('connect-history-api-fallback')
 // ^ middleware to redirect all URLs to index.html
 
 const app = express()
-const staticFileMiddleware = express.static(path.join(__dirname))
+const staticFileMiddleware = express.static(path.join(__dirname, '/dist'))
 
 app.use(staticFileMiddleware)
 app.use(history())
@@ -12,9 +12,9 @@ app.use(staticFileMiddleware)
 // ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'))
+  res.render(path.join(__dirname ,'/dist'))
 })
 
-app.listen(5000, function () {
+app.listen(3000, function () {
   console.log( 'Express serving on 5000!' )
 })
